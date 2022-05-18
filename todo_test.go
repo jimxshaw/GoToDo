c0de86b4c90b72da28fccd93a2c595765b88db6b
Jim Shaw <jimxshaw@gmail.com>
2022-05-17 22:18:58 -0500

    Create TestDelete.

--- todo_test.go
+++ todo_test.go
@@ -17,6 +17,38 @@ func TestAdd(t *testing.T) {
 	}
 }
 
+func TestDelete(t *testing.T) {
+	list := todo.List{}
+
+	tasks := []string{
+		"Do Laundry",
+		"Clean House",
+		"Mow Lawn",
+		"Fold Clothes",
+		"Cook Dinner",
+		"Wash dishes",
+	}
+
+	for _, task := range tasks {
+		list.Add(task)
+	}
+
+	if list[0].Task != tasks[0] {
+		t.Errorf("Expected %q, got %q", tasks[0], list[0].Task)
+	}
+
+	list.Delete(3)
+
+	if len(list) != 5 {
+		t.Errorf("Expected list length of %d but got %d", 5, len(list))
+	}
+
+	if list[2].Task != tasks[3] {
+		t.Errorf("Expected %q but got %q", tasks[3], list[2].Task)
+	}
+
+}
+
 func TestComplete(t *testing.T) {
 	list := todo.List{}
 
