0e3d8925109a2e4e6ac77b4d095ca840a7d8dd72
Jim Shaw <jimxshaw@gmail.com>
2022-05-17 20:36:42 -0500

    Create TestComplete.

--- todo_test.go
+++ todo_test.go
@@ -16,3 +16,24 @@ func TestAdd(t *testing.T) {
 		t.Errorf("Expected %q, got %q", task, list[0].Task)
 	}
 }
+
+func TestComplete(t *testing.T) {
+	list := todo.List{}
+
+	task := "Clean House"
+	list.Add(task)
+
+	if list[0].Task != task {
+		t.Errorf("Expected %q, got %q", task, list[0].Task)
+	}
+
+	if list[0].Done {
+		t.Errorf("New task should not be done but it is.")
+	}
+
+	list.Complete(1)
+
+	if !list[0].Done {
+		t.Errorf("New task should be done but it is not.")
+	}
+}
